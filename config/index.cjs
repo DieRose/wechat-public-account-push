6b26025aec87a03c81b36ca5f29f90c512b3d6b1
DieRose <55175055+DieRose@users.noreply.github.com>
2022-11-21 18:01:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -30,7 +30,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '你', year: '2001', date: '10-06',
+          type: '*生日', name: '你', year: '2001', date: '07-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
